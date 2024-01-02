55d46d149ba9a49e59f024a27aaf66b667420b6a
TieuCaMieu <tieucamieu@gmail.com>
2024-01-02 19:57:10 +0700

    ok

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ dotEnv.config();
 import express from "express";
 const app = express();
 
+app.get("/hello", (req, res) => {
+    return res.status(200).json({
+        message: "Xin chào bạn nhé"
+    })
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server on at: ${process.env.HOST}:${process.env.PORT}`)
